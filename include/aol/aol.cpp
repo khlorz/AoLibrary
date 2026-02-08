304a0cf2436c0bb34c18a596b61339f1b3956863
khlorz <lightningryousei@gmail.com>
2026-02-08 21:55:37 +0800

    Improved aol.cpp comments

--- include/aol/aol.cpp
+++ include/aol/aol.cpp
@@ -1,5 +1,20 @@
-// For stb_sprintf implementation
+/****************************************************************
+* AoLibrary implementations
+* - This houses the implementations needed for the library
+* - e.g stb implementations, mimalloc-new-delete override, etc.
+* - Put all future implementations here [developer]
+****************************************************************/
+
+/***************************
+* stb implementations
+***************************/
+
 #define STB_SPRINTF_IMPLEMENTATION
+
+
+/***************************
+* #includes
+***************************/
 
 #include "aol.h"
 
