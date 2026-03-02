acbdb614db24f3304837705784f6cc107b7bfc58
khlorz <lightningryousei@gmail.com>
2026-03-02 15:37:08 +0800

    Added important includes and spdlog functions/types

--- include/aol/internal/logging.h
+++ include/aol/internal/logging.h
@@ -43,11 +43,29 @@
 *******************************************************/
 
 #include "spdlog/spdlog.h"
+#include "spdlog/async.h"
+#include "spdlog/stopwatch.h"
+
+#include "spdlog/sinks/base_sink.h"
+#include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/rotating_file_sink.h"
+#include "spdlog/sinks/callback_sink.h"
+#include "spdlog/sinks/ostream_sink.h"
+#include "spdlog/sinks/ringbuffer_sink.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/stdout_sinks.h"
 
 namespace AoL
 {
 
+namespace Log
+{
 
+// Since I don't have anything meaningful to add for logging
+// For now, we'll be using the spdlog namespace under AoL::Log
+using namespace spdlog;
+
+}
 
 } // AoL namespace
 
