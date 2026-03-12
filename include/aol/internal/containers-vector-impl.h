3917bc2936f9ab1f36d9d9e96c37b0cb0a1dd967
khlorz <lightningryousei@gmail.com>
2026-03-12 21:54:02 +0800

    Added some traits in VectorCircularEx

--- include/aol/internal/containers-vector-impl.h
+++ include/aol/internal/containers-vector-impl.h
@@ -19,7 +19,21 @@ template<
 >
 struct VectorCircularEx : ContainerBase<ContainerTypeTag::Vector, T, Vector<T,A>>
 {
+private:
+	using Base = ContainerBase<ContainerTypeTag::Vector, T, Vector<T, A>>;
 
+	using Base::container_obj;
+
+public:
+	using container_type = Vector<T,A>;
+
+	using value_type = container_type::value_type;
+	using allocator_type = container_type::allocator_type;
+
+	using pointer = container_type::pointer;
+	using const_pointer = container_type::const_pointer;
+	using reference = container_type::reference;
+	using const_reference = container_type::const_reference;
 };
 
 } // AoL::Internal namespace
