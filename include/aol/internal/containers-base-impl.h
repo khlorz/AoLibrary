bf817d02b34b6c90b56848bbe53f63c9d3abd7ea
khlorz <lightningryousei@gmail.com>
2026-03-13 08:43:13 +0800

    Improved iterators and refactoring

--- include/aol/internal/containers-base-impl.h
+++ include/aol/internal/containers-base-impl.h
@@ -12,6 +12,79 @@ struct AOL_EMPTY_BASE_OPTIMIZATION ContainerTag_KeyOrderMap {};
 struct AOL_EMPTY_BASE_OPTIMIZATION ContainerTag_KeyOrderSet {};
 struct AOL_EMPTY_BASE_OPTIMIZATION ContainerTag_Vector {};
 
+/*************************************************
+* Iterators for the ContainerBase
+* - HasDerivedIteratorImpl checks if the Derived
+*   has its own iterator implementation
+* - IteratorSelector is used for lazy type
+*   instantation for ContainerBase iterator
+*************************************************/
+
+template<typename D>
+concept HasDerivedIteratorImpl = requires
+{
+	D::iterator_impl;
+	D::const_iterator_impl;
+	D::reverse_iterator_impl;
+	D::const_reverse_iterator_impl;
+};
+
+template<typename D, typename C, bool UseDerived>
+struct IteratorSelector
+{
+	using type = typename C::iterator;
+};
+
+template<typename D, typename C>
+struct IteratorSelector<D, C, true>
+{
+	using type = typename D::iterator_impl;
+};
+
+template<typename D, typename C, bool UseDerived>
+struct ConstIteratorSelector
+{
+	using type = typename C::const_iterator;
+};
+
+template<typename D, typename C>
+struct ConstIteratorSelector<D, C, true>
+{
+	using type = typename D::const_iterator_impl;
+};
+
+template<typename D, typename C, bool UseDerived>
+struct ReverseIteratorSelector
+{
+	using type = typename C::reverse_iterator;
+};
+
+template<typename D, typename C>
+struct ReverseIteratorSelector<D, C, true>
+{
+	using type = typename D::reverse_iterator_impl;
+};
+
+template<typename D, typename C, bool UseDerived>
+struct ConstReverseIteratorSelector
+{
+	using type = typename C::const_reverse_iterator;
+};
+
+template<typename D, typename C>
+struct ConstReverseIteratorSelector<D, C, true>
+{
+	using type = typename D::const_reverse_iterator_impl;
+};
+
+
+/*************************************************
+* ContainerBase key and mapped type
+* - This is used to easily create which type are
+*   are used for key and mapped type depending
+*   on the container tag
+*************************************************/
+
 template<typename T, typename Tag>
 struct ContainerKeyType
 {
@@ -42,25 +115,50 @@ struct ContainerMappedType<T, ContainerTag_KeyOrderMap>
 	using type = typename T::second_type;
 };
 
+
+/*************************************************
+* ContainerBase allocator
+* - For non-stl and rot-container, allocator will
+*   be void, which is invalid
+*************************************************/
+
 template<typename C>
 using ContainerAllocatorType = std::conditional_t<Traits::IsSTLContainer<C> || Traits::IsRotContainer<C>, typename C::allocator_type, void>;
 
-template<typename D, typename T, typename C>
-struct ContainerBase<D, T, C, ContainerTag_None>
-{
-	static_assert(Traits::AssertFalse<ContainerTag_None>, "Invalid container tag!");
-};
 
+/*************************************************
+* ContainerBase main implementation
+*************************************************/
+
+/**
+* @details AoLibrary's main container base implementation
+* 
+* - Most of AoLibrary's container will always be derived from this
+* 
+* - This is a CRTP class so it can be a bit of a boilerplate
+* 
+* - Do not create an object of this class!
+* 
+* @tparam D derived class for CRTP purposes
+* @tparam T element type
+* @tparam C container adapted (usually std::vector)
+* @tparam Tag container tag (i.e ContainerTag_XXXX)
+*/
 template<typename D, typename T, typename C, typename Tag>
 struct ContainerBase
 {
+public:
 	using container_tag = Tag;
 	using container_type = C;
 
-	using iterator = typename container_type::iterator;
-	using const_iterator = typename container_type::const_iterator;
-	using reverse_iterator = typename container_type::reverse_iterator;
-	using const_reverse_iterator = typename container_type::const_reverse_iterator;
+	using iterator = IteratorSelector<D, C, HasDerivedIteratorImpl<D>>::type;
+	using const_iterator = ConstIteratorSelector<D, C, HasDerivedIteratorImpl<D>>::type;
+	using reverse_iterator = ReverseIteratorSelector<D, C, HasDerivedIteratorImpl<D>>::type;
+	using const_reverse_iterator = ConstReverseIteratorSelector<D, C, HasDerivedIteratorImpl<D>>::type;
+
+protected:
+	// We use protected ctors and dtor to prevent any creation of this class manually
+	// This way, the derived classes can still use them and still not visible to the user
 
 	ContainerBase() noexcept = default;
 	ContainerBase(const ContainerBase& other) noexcept = default;
@@ -69,33 +167,34 @@ struct ContainerBase
 	ContainerBase& operator = (ContainerBase&& other) = default;
 	~ContainerBase() noexcept = default;
 
-	ContainerBase(SizeT initial_capacity) noexcept :
+	explicit ContainerBase(SizeT initial_capacity) noexcept :
 		container_obj{ }
 	{
 		container_obj.reserve(initial_capacity);
 	}
 
-	ContainerBase(const ContainerAllocatorType<C>& allocator) noexcept :
+	explicit ContainerBase(const ContainerAllocatorType<C>& allocator) noexcept :
 		container_obj{ allocator }
 	{
 	}
 
-	ContainerBase(const container_type& other_data) noexcept :
+	explicit ContainerBase(const container_type& other_data) noexcept :
 		container_obj{ other_data }
 	{
 	}
 
-	ContainerBase(container_type&& other_data) noexcept :
+	explicit ContainerBase(container_type&& other_data) noexcept :
 		container_obj{ std::move(other_data) }
 	{
 	}
 
 	template<typename It> requires std::is_base_of_v<std::input_iterator_tag, typename std::iterator_traits<It>::iterator_category>
-	ContainerBase(It it_start, It it_end) noexcept :
+	explicit ContainerBase(It it_start, It it_end) noexcept :
 		container_obj{ it_start, it_end }
 	{
 	}
 
+public:
 	void clear() noexcept
 	{
 		container_obj.clear();
@@ -106,62 +205,62 @@ struct ContainerBase
 		container_obj.reserve(new_capacity);
 	}
 
-	constexpr auto begin() noexcept
+	constexpr iterator begin() noexcept
 	{
 		return container_obj.begin();
 	}
 
-	constexpr auto begin() const noexcept
+	constexpr const_iterator begin() const noexcept
 	{
 		return container_obj.cbegin();
 	}
 
-	constexpr auto end() noexcept
+	constexpr iterator end() noexcept
 	{
 		return container_obj.end();
 	}
 
-	constexpr auto end() const noexcept
+	constexpr const_iterator end() const noexcept
 	{
 		return container_obj.cend();
 	}
 
-	constexpr auto cbegin() const noexcept
+	constexpr const_iterator cbegin() const noexcept
 	{
 		return container_obj.cbegin();
 	}
 
-	constexpr auto cend() const noexcept
+	constexpr const_iterator cend() const noexcept
 	{
 		return container_obj.cend();
 	}
 
-	constexpr auto rbegin() noexcept
+	constexpr reverse_iterator rbegin() noexcept
 	{
 		return container_obj.rbegin();
 	}
 
-	constexpr auto rend() noexcept
+	constexpr reverse_iterator rend() noexcept
 	{
 		return container_obj.rend();
 	}
 
-	constexpr auto rbegin() const noexcept
+	constexpr const_reverse_iterator rbegin() const noexcept
 	{
 		return container_obj.rbegin();
 	}
 
-	constexpr auto rend() const noexcept
+	constexpr const_reverse_iterator rend() const noexcept
 	{
 		return container_obj.rend();
 	}
 
-	constexpr auto crbegin() const noexcept
+	constexpr const_reverse_iterator crbegin() const noexcept
 	{
 		return container_obj.crbegin();
 	}
 
-	constexpr auto crend() const noexcept
+	constexpr const_reverse_iterator crend() const noexcept
 	{
 		return container_obj.crend();
 	}
@@ -189,6 +288,17 @@ struct ContainerBase
 protected:
 	container_type container_obj;
 
+};
+
+/**
+* @details AoLibrary's main container base implementation
+*
+* - This is the invalid template, whenever ContainerTag is not given
+*/
+template<typename D, typename T, typename C>
+struct ContainerBase<D, T, C, ContainerTag_None>
+{
+	static_assert(Traits::AssertFalse<ContainerTag_None>, "Invalid container tag!");
 };
 
 } // AoL::Internal namespace
