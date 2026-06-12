d99025a4a367a8512f5dbdda12561c5a567070ea
khlorz <lightningryousei@gmail.com>
2026-06-12 18:15:36 +0800

    Refactored FindLowerBound

--- include/aol/internal/algorithm-find-impl.h
+++ include/aol/internal/algorithm-find-impl.h
@@ -26,7 +26,7 @@ concept IsValidLowerBoundType = IsPairType<T> && HasComparableKey<T, K>;
 
 }
 
-// Find a value from a container
+// Find a value from a container using brute force
 // - use std::find for custom size container
 template<typename It, typename T>
 constexpr auto FindBrute(It it_begin, It it_end, T&& val) noexcept
@@ -42,38 +42,36 @@ constexpr auto FindBrute(E&& e, It it_begin, It it_end, T&& val) noexcept requir
 	return std::find(std::forward<E>(e), it_begin, it_end, std::forward<T>(val));
 }
 
-template<typename C, typename K> requires Internal::IsValidLowerBoundType<C, K>
-C* FindLowerBound(const C* p_start, const C* p_end, const K& key)
-{
-    C* p_current = const_cast<C*>(p_start);
-    for (PtrSize current_count = (PtrSize)(p_end - p_current); current_count > 0; )
-    {
-        PtrSize temp_count = current_count >> 1;
-        C* mid = p_current + temp_count;
-        if (mid->first < key)
-        {
-            p_current = ++mid;
-            current_count -= temp_count + 1;
-        }
-        else
-        {
-            current_count = temp_count;
-        }
-    }
-    return p_current;
-}
-
+/*
+* @details Slightly modified lower_bound algorithm
+* 
+* - Requires the container to be sorted, otherwise, it will be UB
+* 
+* - This overload requires the pointer to the container and the pointer to the end of the container
+* 
+* - The value type of the container must have std::pair interface, requiring the member 'first'
+* 
+* - Usually faster than the iterator overload of this function
+* 
+* @tparam It iterator type (can be a pointer)
+* @tparam K key type
+* @tparam Comparator comparison predicate (default: std::less<void>)
+* @param p_start pointer to container address or start
+* @param p_end pointer to container end address
+* @param key value to be found
+* @return iterator to lower bound position for value
+*/
 template<typename It, typename K, typename Comparator = std::less<void>>
 It FindLowerBound(It it_begin, It it_end, const K& value, Comparator comparator = Comparator{})
 {
-    It p_current = it_begin;
-    for (PtrSize current_count = (PtrSize)(it_end - p_current); current_count > 0; )
+    It it_current = it_begin;
+    for (PtrSize current_count = (PtrSize)(it_end - it_current); current_count > 0; )
     {
         PtrSize temp_count = current_count >> 1;
-        It mid = p_current + temp_count;
+        It mid = it_current + temp_count;
         if (comparator(*mid, value))
         {
-            p_current = ++mid;
+            it_current = ++mid;
             current_count -= temp_count + 1;
         }
         else
@@ -81,7 +79,7 @@ It FindLowerBound(It it_begin, It it_end, const K& value, Comparator comparator 
             current_count = temp_count;
         }
     }
-    return p_current;
+    return it_current;
 }
 
 } // AoL namespace
