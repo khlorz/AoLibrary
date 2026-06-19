a02cbee6eb94c7c4416627da88aeb1612a940b4d
khlorz <lightningryousei@gmail.com>
2026-06-19 22:24:12 +0800

    Created random number generator typedefs

--- include/aol/internal/random.h
+++ include/aol/internal/random.h
@@ -19,11 +19,48 @@
 #elif defined(AOL_USE_STD_RNG)
 // By default, <random> is included
 #else
-#error "No custom RNG!"
+// Do nothing
 #endif
 
 namespace AoL
 {
+
+/**
+* @details The 64 bit random number generator
+*/
+using RandGen64
+#if defined(AOL_USE_XOSHIRO_NESSAN_RNG)
+= xso::rng64;
+#elif defined(AOL_USE_PCG_CPP_RNG)
+= pcg64;
+#elif defined(AOL_USE_STD_RNG)
+= std::mt19937_64;
+#else
+#error "No custom random number generator!"
+#endif
+
+/**
+* @details The 32 bit random number generator
+*/
+using RandGen32
+#if defined(AOL_USE_XOSHIRO_NESSAN_RNG)
+= xso::rng32;
+#elif defined(AOL_USE_PCG_CPP_RNG)
+= pcg32;
+#elif defined(AOL_USE_STD_RNG)
+= std::mt19937;
+#else
+#error "No custom random number generator!"
+#endif
+
+/**
+* @details The default RNG generator
+*
+* - By the default, if the config.h is not modified, we use xoshiro implementation
+*
+* - The library can also use pcg-cpp implementation as well
+*/
+using RandGen = RandGen64;
 
 } // AoL namespace
 
