87891d4d74c894864bbd2243a864428ccddd38b5
khlorz <lightningryousei@gmail.com>
2026-06-19 21:46:16 +0800

    Created random.h

---
+++ include/aol/internal/random.h
@@ -0,0 +1,20 @@
+/***************************************************************************************
+* AoLibrary's random
+****************************************************************************************
+* - Contain's the libraries random related things
+* - Planned to contain anything related to randomness
+* - Also contain's the stl's random, aliased to the library for consistency
+***************************************************************************************/
+#ifndef AOL_INTERNAL_RANDOM_H
+#define AOL_INTERNAL_RANDOM_H
+
+#include "config.h"
+
+#include <random>
+
+namespace AoL
+{
+
+} // AoL namespace
+
+#endif // AOL_INTERNAL_RANDOM_H
