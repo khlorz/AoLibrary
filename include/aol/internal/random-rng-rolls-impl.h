d9c7bec222ebd051e24bf36e8d35a8480a73afad
khlorz <lightningryousei@gmail.com>
2026-07-04 18:37:11 +0800

    Adjust RollDice implementations

--- include/aol/internal/random-rng-rolls-impl.h
+++ include/aol/internal/random-rng-rolls-impl.h
@@ -593,70 +593,20 @@ constexpr SignedType RollRangeSlow(SignedType min, SignedType max, RNG& rng, Poo
 * Dice rollings
 ***************************************************/
 
-template<typename IntType, typename RNG, typename Pool>
-constexpr auto RollDiceN(IntType sides, RNG& rng, Pool& pool) noexcept
+template<typename RNG, typename Pool>
+constexpr AoL::SizeT RollDice(AoL::SizeT max_sides, RNG& rng, Pool& pool) noexcept
 {
-	return RollRange(static_cast<IntType>(1), sides, rng, pool);
+	assert(max_sides > 1 && "max_sides should be more than 1");
+
+	return RollRange(static_cast<AoL::SizeT>(1), max_sides, rng, pool);
 }
 
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice6(RNG& rng, Pool& pool) noexcept
+template<AoL::SizeT MaxSides, typename RNG, typename Pool>
+constexpr AoL::SizeT RollDice(RNG& rng, Pool& pool) noexcept
 {
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(6)>(rng, pool);
-}
+	static_assert(MaxSides > 1, "MaxSides should be more than 1!");
 
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice8(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(8)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice10(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(10)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice12(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(12)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice14(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(14)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice16(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(16)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice18(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(18)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice20(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(20)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice22(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(22)>(rng, pool);
-}
-
-template<typename RNG, typename Pool>
-constexpr AoL::U8 RollDice24(RNG& rng, Pool& pool) noexcept
-{
-	return RollRange<static_cast<AoL::U8>(1), static_cast<AoL::U8>(24)>(rng, pool);
+	return RollRange(static_cast<SizeT>(1), MaxSides, rng, pool);
 }
 
 /***************************************************
